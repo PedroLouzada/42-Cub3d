9c73b121d1742622973990a548c1a31551169644
mrapp-he <miguelrapphenriques@gmail.com>
2025-11-28 16:25:43 +0000

    added initilazers for player and enemy

--- lib/classes.h
+++ lib/classes.h
@@ -9,9 +9,10 @@ typedef struct s_player
 	int 	pos_y;
 	int 	lives;
 	bool	collision;
-	void	(*move)(t_player);
-	void	(*damage)(t_player);
-	void	(*render)(t_player);
+	char	orientation;
+	void	(*damage)(t_player*);
+	void	(*render)(t_player*);
+	void	(*move)(t_player*, int, int);
 }	t_player;
 
 typedef struct s_enemy
@@ -19,9 +20,9 @@ typedef struct s_enemy
 	int		pos_x;
 	int		pos_y;
 	bool	collision;
-	void	(*move)(t_enemy);
-	void	(*render)(t_enemy);
-	void	(*respawn)(t_enemy);
+	void	(*render)(t_enemy*);
+	void	(*respawn)(t_enemy*);
+	void	(*move)(t_enemy*, int, int);
 }	t_enemy;
 
 typedef struct s_wall
@@ -29,14 +30,15 @@ typedef struct s_wall
 	bool	door;
 	bool 	collision;
 	char	orientation;
-	void	(*render)(t_wall);
+	void	(*render)(t_wall*);
 }	t_wall;
 
 typedef struct s_map
 {
 	int		width;
 	int		length;
-	void	(*render)(t_map);
+	void	(*render)(t_map*);
+	void	(*resize)(t_map*, int, int);
 }	t_map;
 
 typedef struct s_game
